3b12d36e60c7c1717ec96849d85a5de69d0da9df
Jaemin Seol <43691888+JAEMINSEOL@users.noreply.github.com>
2025-06-12 00:56:31 +0900

    Update 동작구 무료커피 이벤트 멤버십 전환 임펙트 분석.sql

--- 동작구 무료커피 이벤트 멤버십 전환 임펙트 분석.sql
+++ 동작구 무료커피 이벤트 멤버십 전환 임펙트 분석.sql
@@ -1,10 +1,11 @@
 
 select distinct u.user_id, ad.sigungu
-, isnull(p.use_promo, '미발급') ||'-'|| isnull(p.use_promo, '미사용') as 이벤트_커피_사용
+-- , isnull(g.get_promo, '미발급') ||'-'|| isnull(p.use_promo, '미사용') as 이벤트_커피_사용
+, isnull(p.use_promo, '미사용') as 이벤트_커피_사용
 , isnull(a_status, '미가입자')  as 이벤트_전
 , isnull(b_status, '미가입자') as 이벤트_후
-, 이벤트_전 ||'-'|| 이벤트_후 as 이벤트_전_후
-, isnull(c_status, '미가입자') as 이주_뒤
+, 이벤트_전 ||'-'|| 이벤트_후 as 이벤트_전_후_May_16_19
+, isnull(c_status, '미가입자') as 이주_뒤_상태_June_3
 
       
 from doeat_delivery_production.user u
@@ -17,7 +18,7 @@ left join (select user_id, user_type as c_status from doeat_data_mart.mart_membe
 left join(select user_id,'사용' as use_promo from doeat_data_mart.user_log
             where log_type = 'FreePBCoffeeDongjakOpenAlert' and log_action = '결제하기 완료'  and date(created_at) >= '2025-05-17'
             ) p on u.user_id = p.user_id
-left join(select user_id,'발급' as get_promo from doeat_data_mart.user_log
-            where log_type = 'FreePBCoffeeDongjakOpenAlert' and log_action = '이벤트 배너 노출'  and date(created_at) >= '2025-05-17'
-            ) g on u.user_id = g.user_id
+-- left join(select user_id,'발급' as get_promo from doeat_data_mart.user_log
+--             where log_type = 'FreePBCoffeeDongjakOpenAlert' and log_action = '이벤트 오픈 바텀싯 열림'  and date(created_at) >= '2025-05-17'
+--             ) g on u.user_id = g.user_id
 where u.authority = 'GENERAL'
